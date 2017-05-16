b6cfeea7b57c6f8d74de4a50b74b010a21161239
mjtordesillas <mjtordesillas@gmail.com>
2017-05-16 20:59:47 +0200

    refactor: reformat to highlight the 3 A's

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -8,6 +8,7 @@ public class MarsRoverTest {
     @Test
     public void does_nothing_when_receiving_empty_commands_sequence() {
         MarsRover marsRover = new MarsRover(new Coordinates(0,0), "N");
+
         marsRover.receive("");
 
         assertThat(marsRover.getCoordinates(), is(new Coordinates(0,0)));
@@ -17,6 +18,7 @@ public class MarsRoverTest {
     @Test
     public void turning_right_when_pointing_north() {
         MarsRover marsRover = new MarsRover(new Coordinates(0,0), "N");
+
         marsRover.receive("r");
 
         assertThat(marsRover.getCoordinates(), is(new Coordinates(0,0)));
