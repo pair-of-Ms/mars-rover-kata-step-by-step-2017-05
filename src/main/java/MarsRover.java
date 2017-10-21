247673067fc35e952cb09dbc3a747a070bc783e7
trikitrok <trikitrok@gmail.com>
2017-10-21 11:05:02 +0200

    refactor: replaced if-else by switch

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -13,14 +13,19 @@ public class MarsRover {
         if(commandsSequence.isEmpty()) {
             return;
         }
-        if(direction.equals("N")) {
-            direction = "E";
-        } else if(direction.equals("E")){
-            direction = "S";
-        } else if(direction.equals("S")){
-            direction = "W";
-        } else {
-            direction = "N";
+        switch (direction) {
+            case "N":
+                direction = "E";
+                break;
+            case "E":
+                direction = "S";
+                break;
+            case "S":
+                direction = "W";
+                break;
+            default:
+                direction = "N";
+                break;
         }
     }
 
