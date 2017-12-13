bbf0bea860e6332aacde4cbf5494cc9a84b52ce3
mjtordesillas <mjtordesillas@codesai.com>
2017-12-13 19:38:17 +0100

    refactor: using vector to have all commands return the same type

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,25 +1,23 @@
 public class MarsRover {
 
     private static final int MOVEMENT_DELTA = 1;
-    private Coordinates coordinates;
-    private Direction direction;
+    private Vector vector;
 
     public MarsRover(Coordinates coordinates, String direction) {
-        this.setCoordinates(coordinates);
-        this.setDirection(Direction.parse(direction));
+        this.vector = new Vector(coordinates, Direction.parse(direction));
     }
 
     public void receive(String commandsSequence) {
         for(String command:commandsSequence.split(""))
         {
             if (command.equals("r")) {
-                setDirection(getDirection().turnRight());
+                vector = vector.turnRight();
             } else if (command.equals("l")) {
-                setDirection(getDirection().turnLeft());
+                vector = vector.turnLeft();
             } else if (command.equals("f")) {
-                setCoordinates(getDirection().moveForward(getCoordinates(), MOVEMENT_DELTA));
+                vector = vector.moveForward(MOVEMENT_DELTA);
             } else if (command.equals("b")){
-                setCoordinates(getDirection().moveBackwards(getCoordinates(), MOVEMENT_DELTA));
+                vector = vector.moveBackwards(MOVEMENT_DELTA);
             }
         }
     }
@@ -27,44 +25,23 @@ public class MarsRover {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof MarsRover)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
 
         MarsRover marsRover = (MarsRover) o;
 
-        if (getCoordinates() != null ? !getCoordinates().equals(marsRover.getCoordinates()) : marsRover.getCoordinates() != null)
-            return false;
-        return getDirection() == marsRover.getDirection();
-
+        return vector != null ? vector.equals(marsRover.vector) : marsRover.vector == null;
     }
 
     @Override
     public int hashCode() {
-        int result = getCoordinates() != null ? getCoordinates().hashCode() : 0;
-        result = 31 * result + (getDirection() != null ? getDirection().hashCode() : 0);
-        return result;
+        return vector != null ? vector.hashCode() : 0;
     }
 
     @Override
     public String toString() {
         return "MarsRover{" +
-            "coordinates=" + getCoordinates() +
-            ", direction=" + getDirection() +
-            '}';
+                "vector=" + vector +
+                '}';
     }
 
-    private Coordinates getCoordinates() {
-        return coordinates;
-    }
-
-    private void setCoordinates(Coordinates coordinates) {
-        this.coordinates = coordinates;
-    }
-
-    private Direction getDirection() {
-        return direction;
-    }
-
-    private void setDirection(Direction direction) {
-        this.direction = direction;
-    }
 }
